086f89442cf4f14590fde2b1d50964f19fb68200
shamerdanov <shamil332211@list.ru>
2024-06-11 01:58:54 +0400

    add welcomefragment

---
+++ app/src/main/java/com/example/lazaapp/ui/splash/WelcomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.lazaapp.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lazaapp.R
+
+
+class WelcomeFragment : Fragment() {
+
+}
