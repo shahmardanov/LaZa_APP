a838f423ea6154abf202f3a00e1078371148561c
shamerdanov <shamil332211@list.ru>
2024-06-11 03:14:53 +0400

    Add  firebase method

---
+++ app/src/main/java/com/example/lazaapp/di/NetWorkModule.kt
@@ -0,0 +1,17 @@
+package com.example.lazaapp.di
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetWorkModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+}
