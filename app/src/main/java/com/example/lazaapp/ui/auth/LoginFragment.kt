f4b14257a3823e71bf7f125d14d63d23a4a2ff5d
shamerdanov <shamil332211@list.ru>
2024-06-11 02:47:11 +0400

    init

---
+++ app/src/main/java/com/example/lazaapp/ui/auth/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.example.lazaapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lazaapp.R
+import com.example.lazaapp.base.BaseFragment
+import com.example.lazaapp.databinding.FragmentLoginBinding
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
