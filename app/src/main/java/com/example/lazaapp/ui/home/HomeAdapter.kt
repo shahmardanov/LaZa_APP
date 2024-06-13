da0280494aad07f1912ed9a26ef61dad88775052
shamerdanov <shamil332211@list.ru>
2024-06-13 21:09:35 +0400

    add methods in home adapter

--- app/src/main/java/com/example/lazaapp/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/lazaapp/ui/home/HomeAdapter.kt
@@ -1,4 +1,35 @@
 package com.example.lazaapp.ui.home
 
-class HomeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lazaapp.databinding.ItemBrandBinding
+import com.example.lazaapp.model.BrandModel
+
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
+    private val brandList = arrayListOf<BrandModel>()
+
+    inner class HomeViewHolder(val itemBinding: ItemBrandBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val view = ItemBrandBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return brandList.size
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        val brand = brandList[position]
+        holder.itemBinding.brand = brand
+    }
+
+    fun updateList(newList: List<BrandModel>) {
+        brandList.clear()
+        brandList.addAll(newList)
+        notifyDataSetChanged()
+    }
 }
