a13099e81e6ea70acaa9d57aa83b6d762f792b8d
shamerdanov <shamil332211@list.ru>
2024-06-13 21:16:56 +0400

    add method in HomeViewModel class

--- app/src/main/java/com/example/lazaapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/lazaapp/ui/home/HomeViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.lazaapp.ui.home
 
-class HomeViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.lazaapp.R
+import com.example.lazaapp.model.BrandModel
+
+class HomeViewModel : ViewModel() {
+
+    val brandData = MutableLiveData<List<BrandModel>>()
+
+    fun createBrand() {
+        val brandList = listOf(
+            BrandModel(R.drawable.adidas, "Adidas"),
+            BrandModel(R.drawable.nike, "Nike"),
+            BrandModel(R.drawable.fila, "Fila")
+        )
+
+        brandData.value = brandList
+    }
 }
