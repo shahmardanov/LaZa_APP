3733c1a682c4fcaa0feab470253477083e25c942
shamerdanov <shamil332211@list.ru>
2024-06-19 21:45:04 +0400

    init

--- app/src/main/java/com/example/lazaapp/MainActivity.kt
+++ app/src/main/java/com/example/lazaapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.lazaapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun setBottomView() {
+        val manager =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomView, manager.navController)
     }
 }
